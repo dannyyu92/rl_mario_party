11902d0c35f3067bf88ee5a5c64684f26e014ec1
Danny Yu <dannyyu92@gmail.com>
2014-02-06 17:35:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RlMarioParty::Application.config.secret_key_base = 'b17c768000aa3ac6ddbf00b8ed7b9cd6014969f551e45b3a67b3152ae740c6cbe02cb7552b8f4d512c702d1fdd7d6cfaa03aa92ef230b8d51efdeeacacff7e44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RlMarioParty::Application.config.session_store :cookie_store, key: '_rl_mario_party_session'
